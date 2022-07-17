666b054eba3e73bf36e5072a83feae95584b99c7
kamneklogs <kamneklogs@gmail.com>
2022-07-17 01:13:13 -0500

    Added product model

--- ui-service/src/app/models/product.ts
+++ ui-service/src/app/models/product.ts
@@ -1,2 +1,11 @@
 export class Product {
+
+    id?: number;
+    name: string;
+    price: number;
+
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
 }
