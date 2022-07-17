fee8e494dda7708a50d7f950dcfcb7c44c649bd8
kamneklogs <kamneklogs@gmail.com>
2022-07-17 01:29:29 -0500

    Added angular service and changed request type in backend controller

--- fullapp/src/main/java/com/kamneklogs/fullapp/controller/ProductController.java
+++ fullapp/src/main/java/com/kamneklogs/fullapp/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,7 +70,7 @@ public class ProductController {
         return ResponseEntity.ok(new Message("Product saved"));
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody ProductDTO productdDto) {
         if (!productService.existById(id)) {
             return ResponseEntity.notFound().build();
--- ui-service/src/app/service/product.service.ts
+++ ui-service/src/app/service/product.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  productURL = 'http://localhost:8080/product'
+
+  constructor(private httpClient: HttpClient) { }
+
+  public productsList(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.productURL)
+  }
+
+  public productDetailById(id: number): Observable<Product> {
+    return this.httpClient.get<Product>(this.productURL + `/findById/${id}`)
+  }
+
+  public productDetailByName(name: string): Observable<Product> {
+    return this.httpClient.get<Product>(this.productURL + `/findByName/${name}`)
+  }
+
+  public save(product: Product): Observable<any> {
+    return this.httpClient.post<any>(this.productURL, product)
+  }
+
+  public update(id: number, product: Product): Observable<any> {
+    return this.httpClient.put<any>(this.productURL + `/${id}`, product);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(this.productURL + `/${id}`);
+  }
 }
